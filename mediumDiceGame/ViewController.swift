5a4ddf0ed21d69bcccd7f4e614b256b971403f67
Ivan Lai <229705jack2gmail.com>
2021-05-20 13:43:15 +0800

    Initial Commit

---
+++ mediumDiceGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mediumDiceGame
+//
+//  Created by Lai Po Ying on 2021/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
